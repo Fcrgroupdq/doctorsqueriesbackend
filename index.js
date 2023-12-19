cd717d6053a8eeef29a087e9cb3cc395d1af3f91
pawan kumar <pawan6200327812@gmail.com>
2023-12-20 00:11:07 +0530

    done

--- index.js
+++ index.js
@@ -19,11 +19,19 @@ const { BannerRoute } = require("./routes/banner.route");
 const server = express();
 
 server.use(express.json());
+
 server.use(cors({
   origin: ['http://localhost:3000', 'https://www.doctorsqueries.com/'], // Add multiple origins here
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true // Enable credentials (if needed)
 }));
+
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.doctorsqueries.com');
+  // Add other CORS headers if needed
+  // ...
+  next();
+});
 
 // Function to fetch the logo image
 function fetchImage(imageUrl, callback) {
