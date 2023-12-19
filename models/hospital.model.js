a986bbfd3eb8728d97bacbe785c82eb3232bcf24
pawan kumar <pawan6200327812@gmail.com>
2023-12-19 23:40:49 +0530

    done

--- models/hospital.model.js
+++ models/hospital.model.js
@@ -2,24 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const HospitalSchema = mongoose.Schema({
-    image1:{required:true,type:String},
-    image2:{required:true,type:String},
+    image:{required:true,type:String},
     name:{required:true,type:String},
     location:{required:true,type:String},
     established:{required:true,type:Number},
     beds:{required:true,type:Number},
     about:{required:true,type:String},
-    feature1:{required:true,type:String},
-    feature2:{required:true,type:String},
-    feature3:{required:true,type:String},
-    feature4:{required:true,type:String},
-    feature5:{type:String},
-    feature6:{type:String},
-    feature7:{type:String},
-    feature8:{type:String},
-    feature9:{type:String},
-    feature10:{type:String},
-    isPremium:{type:Boolean}
+    features:{required:true,type:String},
+    specialist:{required:true,type:String}
 },{
     versionKey:false
 })
