8d675fbdc9cedf1ce43f82a9061a5ef038068416
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-10 23:53:38 +0530

    Update appointment.route.js

--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -29,19 +29,12 @@ AppointmentRoute.get("/", async (req, res) => {
 
 AppointmentRoute.post("/", async (req, res) => {
   try {
-    // const { token } = req.body;
-    // const decode = jwt.verify(token, "solo_project");
-    // if (true) {
-      // req.body.userId = decode.userID;
-      // req.body.userId = null;
-      // delete req.body.token;
+  
       
       const newAppointment = new AppointMentModel(req.body);
       const savedAppointment = await newAppointment.save();
       res.status(201).json({ msg: "Appointment saved successfully" });
-    // } else {
-    //   res.send({ msg: "Please Login !!!" });
-    // }
+  
   } catch (error) {
     console.error("Error while creating an appointment:", error);
     res.status(500).json({ msg: "Failed to create an appointment" });
