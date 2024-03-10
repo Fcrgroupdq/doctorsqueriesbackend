19e81e2bee3d461b3dfb26c5eccbf88c973a5087
pawan kumar <pawan6200327812@gmail.com>
2024-03-11 00:32:13 +0530

    uppdated

--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -29,18 +29,18 @@ AppointmentRoute.get("/", async (req, res) => {
 
 AppointmentRoute.post("/", async (req, res) => {
   try {
-    const { token } = req.body;
-    const decode = jwt.verify(token, "solo_project");
-    if (decode) {
-      req.body.userId = decode.userID;
-      delete req.body.token;
+    // const { token } = req.body;
+    // const decode = jwt.verify(token, "solo_project");
+    // if (decode) {
+    //   req.body.userId = decode.userID;
+    //   delete req.body.token;
       
       const newAppointment = new AppointMentModel(req.body);
       const savedAppointment = await newAppointment.save();
       res.status(201).json({ msg: "Appointment saved successfully" });
-    } else {
-      res.send({ msg: "Please Login !!!" });
-    }
+    // } else {
+    //   res.send({ msg: "Please Login !!!" });
+    // }
   } catch (error) {
     console.error("Error while creating an appointment:", error);
     res.status(500).json({ msg: "Failed to create an appointment" });
