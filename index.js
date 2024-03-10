7090d1b6f789bf84d7c55232521af64ad7b6628f
Fcrgroupdq <137594100+Fcrgroupdq@users.noreply.github.com>
2024-03-10 22:04:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,8 @@ const server = express();
 
 
 server.use(express.json());
-// server.use(cors());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.doctorsqueries.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+server.use(cors());
+
 server.use(express.urlencoded({extended:true}));
 
 
